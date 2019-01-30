13412a43bdc6af8d0aff064be62db1184f6d1805
Ariel Magbanua <ariel.magbanua@zeald.com>
2019-01-30 14:21:58 +0800

    Installed vue js app template.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <message>Hello there</message>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
